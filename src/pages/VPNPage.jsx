c4fec5cd8be1331039e3506a0bb214821a4d048c
Stanley Offor <offorstanley4real@gmail.com>
2024-09-13 15:28:24 +0100

    vpn service details page completed

---
+++ src/pages/VPNPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Box } from "@mui/material";
+import VPNDetails from "../components/ServiceDetail/VPNDetails";
+import Footer from "../components/Footer";
+
+
+
+
+
+const VPNPage = () => {
+    return(
+        <Box>
+            <Box>
+                <VPNDetails />
+            </Box>
+            <Box>
+                <Footer />
+            </Box>
+        </Box>
+    )
+}
+
+export default VPNPage;
