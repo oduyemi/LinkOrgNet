58843520daf9ee049fc3c4521e27ddf296a54cb4
Stanley Offor <offorstanley4real@gmail.com>
2024-09-25 13:41:55 +0100

    made changes ti internet component

--- src/components/ServiceDetail/InternetDetails.jsx
+++ src/components/ServiceDetail/InternetDetails.jsx
@@ -4,11 +4,11 @@ import ServiceDetail from ".";
 const serviceData = {
   title: "Internet Services",
   imageSrc: require("../../assets/images/services/internet.jpg"),
-  imageAlt: "Internet Service",
+  imageAlt: "Internet Solutions",
   description: `<p>At LinkOrg Networks, we offer advanced Internet solutions to enhance your digital experience. Our high-speed wireless internet delivers exceptional speed and reliability, eliminating slow connections. For even faster performance, our fiber-optic speeds provide ultra-fast data transmission suitable for both residential and business use.</p>
   <p>Additionally, our community Internet solutions improve network access across residential estates, ensuring every household benefits from a strong, high-speed connection. Our services are designed to support seamless streaming, efficient work, and robust community Internet.</p>`,
 
-  subtitle: "Internet Services we Offer",
+  subtitle: "Internet Solutions we Offer",
 
   carouselItems: [
     { imgSrc: require("../../assets/images/services/connect1.jpg"), title: "High-Speed Wireless", description: "Stay connected with fast, reliable wireless internet." },
